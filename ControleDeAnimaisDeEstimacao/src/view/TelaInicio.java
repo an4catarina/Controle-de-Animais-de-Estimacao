72547284aa933d856df8e55fc89a865a27a078aa
Ana Catarina Santos <an4catarina@gmail.com>
2023-01-13 19:33:07 -0300

    mudando a versão do java

--- ControleDeAnimaisDeEstimacao/src/view/TelaInicio.java
+++ ControleDeAnimaisDeEstimacao/src/view/TelaInicio.java
@@ -2,8 +2,5 @@ package view;
 import javax.swing.*;
 
 public class TelaInicio {
-	public static void main ( String [] a ) {
-		JFrame f = new JFrame (" Hello world!" );
-		f. setVisible ( true );
-	}
+
 }
