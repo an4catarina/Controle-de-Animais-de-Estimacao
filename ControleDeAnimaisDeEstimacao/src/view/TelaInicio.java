11d80f06299d0850786f7fe873ae5718f3688c7e
Ana Catarina Santos <an4catarina@gmail.com>
2023-01-13 11:03:32 -0300

    iniciando a criação da interface

---
+++ ControleDeAnimaisDeEstimacao/src/view/TelaInicio.java
@@ -0,0 +1,9 @@
+package view;
+import javax.swing.*;
+
+public class TelaInicio {
+	public static void main ( String [] a ) {
+		JFrame f = new JFrame (" Hello world!" );
+		f. setVisible ( true );
+	}
+}
